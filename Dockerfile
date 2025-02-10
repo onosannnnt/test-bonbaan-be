b067641dd43fa560cb9b379bc34f3e4b12d2d3d4
onosannnnt <onopingping@gmail.com>
2025-02-10 17:57:38 +0700

    Update Dockerfile and main.go to use godotenv for environment variable management

--- Dockerfile
+++ Dockerfile
@@ -1,35 +1,13 @@
-# -------- Stage 1: Build the Go app --------
-FROM golang:1.21 AS builder
+# Choose whatever you want, version >= 1.16
+FROM golang:1.23-alpine
 
-# Set the working directory
 WORKDIR /app
 
-# Copy go.mod and go.sum first (better for caching)
-COPY go.mod go.sum ./
+RUN go install github.com/air-verse/air@latest
 
-# Download dependencies
+COPY go.mod go.sum ./
 RUN go mod download
 
-# Copy the source code
 COPY . .
 
-# Build the Go application
-RUN CGO_ENABLED=0 GOOS=linux go build -o main
-
-# -------- Stage 2: Create a small runtime image --------
-FROM alpine:latest
-
-# Install required certificates for HTTPS (if needed)
-RUN apk --no-cache add ca-certificates
-
-# Set working directory
-WORKDIR /root/
-
-# Copy the built binary from the builder stage
-COPY --from=builder /app/main .
-
-# Expose the application's port (change if needed)
-EXPOSE 3000
-
-# Run the application
-CMD ["./main"]
+CMD ["air"]
--- main.go
+++ main.go
@@ -3,18 +3,49 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"path/filepath"
+	"runtime"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 	"github.com/omise/omise-go"
 	"github.com/omise/omise-go/operations"
 )
 
-const (
-	OmisePublicKey = "pkey_test_62o41s4lh9tra5ygw1t"
-	OmiseSecretKey = "skey_test_62o41s51mezhmihco72"
+var (
+	OmisePublicKey string
+	OmiseSecretKey string
 )
 
 func main() {
+	_, filename, _, _ := runtime.Caller(0)
+	currentFileDir := filepath.Dir(filename)
+
+	// Walk up the directory tree to find the .env file
+	var configPath string
+	for {
+		configPath = filepath.Join(currentFileDir, ".env")
+		if _, err := os.Stat(configPath); !os.IsNotExist(err) {
+			break
+		}
+		parentDir := filepath.Dir(currentFileDir)
+		if parentDir == currentFileDir {
+			log.Fatalf(".env file not found")
+			os.Exit(-1)
+		}
+		currentFileDir = parentDir
+	}
+
+	// Load the .env file
+	err := godotenv.Load(configPath)
+	if err != nil {
+		log.Fatalf("Problem loading .env file: %v", err)
+		os.Exit(-1)
+	}
+	OmisePublicKey = os.Getenv("OMISE_PUBLIC_KEY")
+	OmiseSecretKey = os.Getenv("OMISE_SECRET_KEY")
+	fmt.Println("OmisePublicKey:", OmisePublicKey)
 	app := fiber.New()
 
 	app.Post("/webhook", handleWebhook)
